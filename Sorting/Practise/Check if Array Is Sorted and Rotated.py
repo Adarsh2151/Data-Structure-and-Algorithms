2f52b97069d1548b263acd334c57630f45558d9b
adarsh <adarshkumar2151@gmail.com>
2022-07-02 15:32:43 +0530

    Adding the code and concept of check the rotated sorted array on the principle of pivot

---
+++ Sorting/Practise/Check if Array Is Sorted and Rotated.py
@@ -0,0 +1,19 @@
+"""
+Given a rotated array you have to find whethear it is sorted or not 
+
+
+My Approach to solve it 
+- Find how many pivot index are there. By using a logic 'if nums[i-1] > nums[i]' means that if successor is less then count it.
+
+
+
+"""
+
+def check( nums):
+    count = sum(1 for i in range(len(nums)) if nums[i-1] > nums[i])
+    if count <= 1:
+        return True
+
+if __name__ == "__main__":
+    arr = [5,6,7,4,3,2]
+    print()
